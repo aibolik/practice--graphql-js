fb9b904cbb455d36454f82d82785c396cfce4044
Aibol Kussain <aibolikdev@gmail.com>
2018-08-18 19:44:18 +0300

    feat: implement voting functionality with Subscription and Mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -78,10 +78,30 @@ async function login(parent, args, context, info) {
   }
 }
 
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context)
+
+  const linkExists = await context.db.exists.Vote({
+    user: { id: userId },
+    link: { id: args.linkId }
+  })
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  return context.db.mutation.createVote({
+    data: {
+      user: { connect: { id: userId } },
+      link: { connect: { id: args.linkId } }
+    }
+  }, info)
+}
+
 module.exports = {
   post,
   updateLink,
   deleteLink,
   signup,
-  login
+  login,
+  vote
 }
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,10 +1,14 @@
 function newLinkSubscribe(parent, args, context, info) {
-  console.log('debug:context.db:', context.db)
-  console.log('debug:context.db.subscription', context.db.subscription)
-  console.log('debug:context.db.subscription.link', context.db.subscription.link)
   return context.db.subscription.link(
     { where: { mutation_in: ['CREATED'] } },
-    info,
+    info
+  )
+}
+
+function newVoteSubscribe(parent, args, context, info) {
+  return context.db.subscription.vote(
+    { where: { mutation_in: ['CREATED'] } },
+    info
   )
 }
 
@@ -12,6 +16,11 @@ const newLink = {
   subscribe: newLinkSubscribe
 }
 
+const newVote = {
+  subscribe: newVoteSubscribe
+}
+
 module.exports = {
-  newLink
+  newLink,
+  newVote
 }
