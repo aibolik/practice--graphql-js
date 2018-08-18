515aa1bb03367d0ddc8188e4b587c865a45e7941
Aibol Kussain <aibolikdev@gmail.com>
2018-08-18 07:56:10 +0300

    feat: implement simple Link Object type and its operations

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of Hackernews Clone`
+    info: () => `This is the API of Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url
   }
 }
 
