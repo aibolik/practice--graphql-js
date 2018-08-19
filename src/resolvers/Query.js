fc9b2b4a5c501609903dc944e406a197fd7cbdf8
Aibol Kussain <aibolikdev@gmail.com>
2018-08-19 07:48:54 +0300

    feat: add more filtering, pagination and sorting for Feed object type

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,31 @@
-function feed(root, args, context, info) {
-  return context.db.query.links({}, info)
+async function feed(root, { filter, first, skip, orderBy }, context, info) {
+  const where = filter 
+    ? {
+      OR: [
+        { url_contains: filter },
+        { description_contains: filter }
+      ]
+    }
+    : {}
+
+  const queriedLinks = await context.db.query.links(
+    { where, first, skip, orderBy }, `{ id }`
+  )
+
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `
+
+  const linksConnections = await context.db.query.linksConnection({}, countSelectionSet)
+
+  return {
+    count: linksConnections.aggregate.count,
+    linkIds: queriedLinks.map(link => link.id)
+  }
 }
 
 function link(root, { id }, context, info) {
